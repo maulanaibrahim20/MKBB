4f340d78a3c5532b03f6978362d61cd26750f2ee
Miftah28 <miftahussurur280602@gmail.com>
2024-06-28 00:09:38 +0700

    gambar

--- app/Helpers/MasterHelpers.php
+++ app/Helpers/MasterHelpers.php
@@ -1,11 +1,20 @@
 <?php
 
 use App\Models\Blog;
+use App\Models\Gambar;
 
 if (!function_exists('blogs')) {
     function blogs()
     {
         $results = Blog::where('id', 1)->first();
+        return $results;
+    }
+}
+
+if (!function_exists('gambarproduk')) {
+    function gambarproduk($idproduk)
+    {
+        $results = Gambar::where('produk_id', $idproduk)->get();
         return $results;
     }
 }
